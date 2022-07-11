fd52fffb4b5655d587eddf7a4de17f4b6fddd6d3
anamariaDobroiu <108723887+anamariaDobroiu@users.noreply.github.com>
2022-07-11 17:32:31 +0300

    Add files via upload
    Code challenge: Fizz Buzz
    Difficulty: Easy
    Write a function that counts from 1 through 100, and prints “Fizz” if the counter is evenly divisible by 3, “Buzz” if it’s evenly divisible by 5, “Fizz Buzz” if it’s even divisible by three and five, or the counter number for all other cases.
    Sample input and output
    1 should print “1”
    2 should print “2”
    3 should print “Fizz”
    4 should print “4”
    5 should print “Buzz”
    15 should print “Fizz Buzz”

---
+++ Day6.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+
+func fizzbuzz ()->Void
+{
+    for i in 1...100{
+        switch (i % 3 == 0, i % 5 == 0)
+        {
+        case (true, false):
+        print("Fizz")
+        case (false, true):
+        print("Buzz")
+        case (true, true):
+        print("FizzBuzz")
+        default:
+        print(i)
+        }
+}
+}
+fizzbuzz()
+
